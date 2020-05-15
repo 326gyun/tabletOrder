118758bf45818e73dfb44aae2863515b6cf73d06
326gyun <65344678+326gyun@users.noreply.github.com>
2020-05-15 10:39:30 +0900

    Add files via upload

---
+++ TestEx.java
@@ -0,0 +1,30 @@
+package test;
+
+public class TestEx {
+
+	public static void main(String[] args) {
+		
+		System.out.println("æ»≥Á«œººø‰.");
+		
+		int num = 60;
+		switch(num/10) {
+		case 10:
+		case 9:
+			System.out.println("A");
+			break;
+		case 8:
+			System.out.println("B");
+			break;
+		case 7:
+			System.out.println("C");
+			break;
+		case 6:
+			System.out.println("D");
+			break;
+			default:
+				System.out.println("F");
+		}
+
+	}
+
+}
